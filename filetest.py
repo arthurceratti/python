bbd04d187611c0968737deb35fd162a5fd4f29ab
Arthur Ceratti <arthurceratti@gmail.com>
2020-04-23 12:38:34 -0300

    Versão limpa

--- filetest.py
+++ filetest.py
@@ -1,5 +1,4 @@
 #!/bin/python3
-
 import math
 import os
 import random
@@ -8,37 +7,39 @@ import sys
 import serial
 import matplotlib.pyplot as plt
 
-def humidityShowFunction(): 
-    plt.plot(xAxysList, humidity_list)
-    plt.show() 
-def temperatureShowFunction():
-    plt.plot(xAxysList, temperatureList)
-    plt.show() 
-
-t = 0
-humidity_list = []
-temperatureList = []
-xAxysList = []
-port = serial.Serial("COM4", baudrate=115200, timeout=3.0)
-rcv = [0]
-while t<10:
-    #port.write("\r\nSay something:")
-    # buffer de x tamanho
-    rcv = port.read(100)
-    #decodificando de byte para ASCII
-    rcv=rcv.decode('ASCII')
-    humidity = int(rcv[0:2])
-    temperature = int(rcv[5:7])
-    #print(rcv[5:7])
-    print(t)
-    humidity_list.append(humidity)
-    temperatureList.append(temperature)
-    xAxysList.append(t)
-    t = t+1
-#humidityShowFunction()    
-temperatureShowFunction()
-#print(humidity_list)
-    #port.write("\r\nYou sent:" + repr(rcv))
 #f= open("guru99.txt","w+")
 #fh = open("welcome.txt","r")
 #print (f.read())
+
+def plotFunctionList(x,y): 
+    plt.plot(x, y)
+    #plt.pause(0.05)
+    plt.show() 
+
+#Variable declarations
+humidity_list = []
+temperatureList = []
+xAxysList = []
+rcv = [0]
+
+#Setup Serial Port
+port = serial.Serial("COM4", baudrate=115200, timeout=3.0)
+rcv = port.read(100)
+rcv = rcv.decode('ASCII')
+port.write(79)
+ 
+t=0 #numero de amostras de temperatura e umidade antes de plotar
+while t<10:
+    # buffer de x tamanho
+    port.write(75)
+    rcv = port.read(100)
+    rcv = rcv.decode('ASCII')
+    #decodificando de byte para ASCII
+    temperature = int(rcv[0:2])
+    print(t)
+    #humidity_list.append(humidity)
+    temperatureList.append(temperature)
+    xAxysList.append(t)
+    t = t+1
+plotFunctionList(xAxysList,temperatureList)
+
